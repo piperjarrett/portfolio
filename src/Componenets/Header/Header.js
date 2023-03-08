ec2313d7a53151d9804c35d8d29423e92861015e
Piper Jarrett <piperjarrett@pipers-mbp.lan>
2023-03-08 15:00:17 -0700

    add download of cv

--- src/Componenets/Header/Header.js
+++ src/Componenets/Header/Header.js
@@ -3,6 +3,20 @@ import { NavLink } from "react-router-dom";
 import "./Header.css";
 
 const Header = () => {
+  const downloadCV = () => {
+    // using Java Script method to get PDF file
+    // fetch().then((response) => {
+    //   response.blob().then((blob) => {
+    //     // Creating new object of PDF file
+    //     const fileURL = window.URL.createObjectURL(blob);
+    //     // Setting various property values
+    //     let alink = document.createElement("a");
+    //     alink.href = fileURL;
+    //     alink.download = "Piper Jarrett Resume.pdf";
+    //     alink.click();
+    //   });
+    // });
+  };
   return (
     <Heading>
       <p>Piper Jarrett</p>
@@ -15,7 +29,9 @@ const Header = () => {
       <NavLink to="/projects">
         <p>Projects</p>
       </NavLink>
-      <p>Download CV</p>
+      <a href={process.env.PUBLIC_URL + "Piper Jarrett Resume.pdf"} download>
+        Download CV
+      </a>
     </Heading>
   );
 };
