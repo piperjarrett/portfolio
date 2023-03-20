4a0528f3b7afe547f55fc1f2fcac519fd67d9a29
Piper Jarrett <piperjarrett@pipers-mbp.lan>
2023-03-20 13:23:15 -0600

    add home info

--- src/Componenets/Home/Home.js
+++ src/Componenets/Home/Home.js
@@ -1,9 +1,57 @@
 import "./Home.css";
+import profileImg from "../../assets/profileImg.JPG";
+import {
+  Card,
+  Image,
+  Stack,
+  CardBody,
+  Text,
+  Button,
+  Heading,
+  CardFooter,
+} from "@chakra-ui/react";
 // import profileImg from "..../public/assets/profileImg";
 const Home = () => {
   return (
     <div className="home">
-      <div className="image">
+      <Card
+        direction={{ base: "column", sm: "row" }}
+        overflow="hidden"
+        variant="outline"
+      >
+        <Image
+          objectFit="cover"
+          maxW={{ base: "100%", sm: "200px" }}
+          src={profileImg}
+          alt="piper Jarrett image"
+        />
+
+        <Stack>
+          <CardBody>
+            <Heading size="md">Piper Jarrett</Heading>
+
+            <Text py="2">
+              Software Developer and Problem Solving Enthusiast with a passion
+              for finding the most efficient and effective solution to complex
+              problems. With a background in Mechanical Engineering, I bring a
+              unique perspective and approach to software development.
+            </Text>
+          </CardBody>
+
+          <CardFooter>
+            <Button variant="solid" colorScheme="blue">
+              Github
+            </Button>
+            <Button variant="solid" colorScheme="blue">
+              Linkedin
+            </Button>
+            <Button variant="solid" colorScheme="blue">
+              Email
+            </Button>
+          </CardFooter>
+        </Stack>
+      </Card>
+      {/* <div className="image">
         <img src="https://photos.google.com/search/_tra_/photo/AF1QipOqEiVbGtJ3lYrkedpYoP1yMzuCyT-aSlO8bBnB" />
       </div>
       <div className="info">
@@ -15,7 +63,7 @@ const Home = () => {
           With a background in Mechanical Engineering, I bring a unique
           perspective and approach to software development.
         </p>
-      </div>
+      </div> */}
     </div>
   );
 };
